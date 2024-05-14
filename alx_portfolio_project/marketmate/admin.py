d11858042c7bb782b9a72e9317f2c9bed53b855a
OscarM03 <oscarmutwiri03@gmail.com>
2024-05-14 08:27:45 +0300

    Registering products to admin dashboard

--- alx_portfolio_project/marketmate/admin.py
+++ alx_portfolio_project/marketmate/admin.py
@@ -1,7 +1,9 @@
+"""Import necessary modules and functions from Django"""
 from django.contrib import admin
-from .models import Category, Business, Product
+from .models import Category, Business, Product, User
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Business)
 admin.site.register(Product)
